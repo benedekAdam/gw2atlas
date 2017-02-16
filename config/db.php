1da3712e658eb595be90274a0be3c8173da8e75b
Benedek Ádám <benedek.adam@introweb.hu>
2017-02-16 20:54:16 +0100

    Added Yii2 framework
    Hey, I'm still learning, so I hope it can help.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=gw2atlas',
+    'username' => 'gw2atlas',
+    'password' => 'Game123staR',
+    'charset' => 'utf8',
+];
--- index.php
+++ index.php
@@ -6,3 +6,18 @@
  * and open the template in the editor.
  */
 
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+// register Composer autoloader
+require(__DIR__ . '/vendor/autoload.php');
+
+// include Yii class file
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+
+// load application configuration
+$config = require(__DIR__ . '/config/web.php');
+
+// create, configure and run application
+(new yii\web\Application($config))->run();
+
